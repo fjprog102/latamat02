885849c314b9f7c3f35da0043f96991b41a91a78
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-07 13:23:52 -0300

    Added solution for descendingorder kata

---
+++ katas/descendingorder/adriel/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Type a non-negative number");
+string input = Console.ReadLine();
+
+long checkInt;
+if(!long.TryParse(input, out checkInt) || checkInt < 0)
+{
+    System.Console.WriteLine("Only non-negative numbers accepted.");
+    return;
+}
+
+char[] res = input.ToArray();
+Array.Sort(res);
+Array.Reverse(res);
+String.Join("", res);
+
+System.Console.WriteLine(res);
