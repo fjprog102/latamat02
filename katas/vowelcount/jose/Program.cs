a1095b575e282d35c1b7e5c0db4d8b4495937a66
Jose Guevara <Jose.Guevara@Jala.University>
2022-11-07 09:33:21 -0400

    Add isVowel() function with basic input

---
+++ katas/vowelcount/jose/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Write Input:");
+var input = Console.ReadLine();
+
+Console.WriteLine($"Is vowel: {isVowel(input[0])}");
+
+bool isVowel(char letter) {
+    return letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u';
+}
