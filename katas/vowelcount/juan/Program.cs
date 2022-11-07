c864d30d69dfcc295d81b5f6ec5d72ff992f05c1
Schlvf <axlzbot@gmail.com>
2022-11-07 14:50:02 -0500

    Added count-vowels kata for Juan

---
+++ katas/vowelcount/juan/Program.cs
@@ -0,0 +1,18 @@
+﻿char[] user_input = Console.ReadLine().ToArray();
+Int32 opt = 0;
+foreach (char val in user_input)
+{
+    if (is_vowel(val))
+    {
+        opt += 1;
+    }
+}
+
+Console.WriteLine(opt);
+
+bool is_vowel(Char val)
+{
+    Char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+    return vowels.Contains(Char.ToLower(val));
+}
