f746f5726e648d33c4c28b4075e27f1bbc0c9010
Beyta Gonzales <svaleria.gonzales@gmail.com>
2022-11-07 14:53:39 -0400

    Added highestlowest kata resolution for Valeria

---
+++ katas/highestlowest/valeria/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    public class Class1
+    {
+        public static string highAndLow(string s)
+        {
+            var array = Array.ConvertAll(s.Split(' '), int.Parse);
+            return array.Max() + " " + array.Min();
+        }
+    }
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.highAndLow("1 2 3 4 5"));
+            Console.WriteLine(Class1.highAndLow("1 2 -3 4 5"));
+            Console.WriteLine(Class1.highAndLow("1 9 3 4 -5"));
+        }
+    }
+}
