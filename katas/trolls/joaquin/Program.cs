70f631ba629404c3e6d8f41cab6ab02e3673e3a0
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-11-07 20:45:07 -0300

    Add trolls kata resolution for Joaquin

---
+++ katas/trolls/joaquin/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string removeVowels(string input) {
+
+    string output = "";
+    char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
+    for (int i=0; i<input.Length; i++) {
+        
+        if (!vowels.Contains(Char.ToLower(input[i]))) {
+            output += input[i];
+        }
+    }
+
+    return output;
+
+}
+
+
+Console.WriteLine(removeVowels("This website is for losers LOL!"));
