6b4325c4fb155aa1af55dc6c03218278d9bfde26
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-07 18:18:29 -0300

    Added Trolls kata resolution for Adriel

---
+++ katas/trolls/adriel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+string trollTheTrolls(string s)
+{
+    Regex rx = new Regex("[a|e|i|o|u]", RegexOptions.IgnoreCase);
+     return rx.Replace(s, "");
+}
+
+Console.WriteLine(trollTheTrolls("This website is for losers LOL!"));
+Console.WriteLine(trollTheTrolls("This video is horrible!"));
+Console.WriteLine(trollTheTrolls("Didn't like this at all!"));
