a5078e4aba4fa45bd26d1f949349c8070a4c5aa9
Schlvf <axlzbot@gmail.com>
2022-11-07 14:45:26 -0500

    Added trolls kata for Juan

---
+++ katas/trolls/juan/Program.cs
@@ -0,0 +1,18 @@
+﻿char[] user_input = Console.ReadLine().ToArray();
+String opt = "";
+foreach (char val in user_input)
+{
+    if (!is_vowel(val))
+    {
+        opt += val.ToString();
+    }
+}
+
+Console.WriteLine(opt);
+
+bool is_vowel(Char val)
+{
+    Char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+    return vowels.Contains(Char.ToLower(val));
+}
