677cbb4af712ed0ae92c804dde1b728ad85599bb
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-07 18:34:23 -0300

    Added Vowel Count kata resolution for Adriel

---
+++ katas/vowelcount/adriel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+int vowelCount(string s)
+{
+    Regex rx = new Regex("[a|e|i|o|u]", RegexOptions.IgnoreCase);
+     return rx.Matches(s).Count;
+}
+
+System.Console.WriteLine(vowelCount("aaaajjj"));
+System.Console.WriteLine(vowelCount("akfjuehdgyua"));
+System.Console.WriteLine(vowelCount("This is a test for the function"));
