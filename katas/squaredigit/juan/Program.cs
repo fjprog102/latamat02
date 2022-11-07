0d04b4b2d88c34a3161f2ec63485df110dbc1118
Schlvf <axlzbot@gmail.com>
2022-11-07 14:33:52 -0500

    Added square-digit kata for Juan

---
+++ katas/squaredigit/juan/Program.cs
@@ -0,0 +1,16 @@
+﻿char[] user_input = Console.ReadLine().ToArray();
+
+String opt = "";
+
+foreach (char val in user_input)
+{
+    opt += get_square(val);
+}
+Console.WriteLine(opt);
+
+String get_square(char value)
+{
+    int parsed = Int32.Parse(value.ToString());
+    int square = parsed * parsed;
+    return square.ToString();
+}
