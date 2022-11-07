36e842f24e34d32d5163639cd26504029057f695
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-07 15:32:46 -0300

    Added highestlowest kata solution for Adriel

---
+++ katas/highestlowest/adriel/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Type specific string:");
+string input = Console.ReadLine();
+string[] splitInput = input.Split(' ');
+
+List<int> inputList = new List<int>();
+foreach (string s in splitInput)
+{
+    inputList.Add(Convert.ToInt32(s));
+}
+inputList.Sort();
+
+Console.WriteLine($"{inputList.Last()} {inputList.First()}");
