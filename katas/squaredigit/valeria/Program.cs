82a253b38938cab8b722fe1fc9e3fc75fe2a3938
Beyta Gonzales <svaleria.gonzales@gmail.com>
2022-11-07 18:45:59 -0400

    Added Square Digit kata resolution for Valeria

---
+++ katas/squaredigit/valeria/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MyConsoleApp
+{
+    public class Class1
+    {
+        public static string SquareDigit(string s)
+        {
+            string res = "a valid number is required";
+            Regex regex = new Regex(@"^\d+$");
+            if(regex.IsMatch(s)){
+                res = "";
+                foreach (char item in s)
+                {
+                    int sqr = (int)Char.GetNumericValue(item);
+                    sqr = sqr * sqr;
+                    res += sqr.ToString();
+                }
+            }
+            return res;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.SquareDigit(Console.ReadLine()));
+        }
+    }
+}
