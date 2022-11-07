3759f5f98c5f4c32aea95891e3c3dbeea7ada529
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-07 17:52:22 -0300

    Added Square Digit kata resolution for Adriel

---
+++ katas/squaredigit/adriel/Program.cs
@@ -0,0 +1,15 @@
+﻿int squareEveryDigit(int num)
+{
+    List<int> numberList = new List<int>();
+    while (num != 0) {
+        numberList.Insert(0, (num % 10) * (num % 10));
+        num = num / 10;
+    }
+
+    return Convert.ToInt32(String.Join("", numberList));
+
+}
+
+System.Console.WriteLine(squareEveryDigit(123));
+System.Console.WriteLine(squareEveryDigit(3451));
+System.Console.WriteLine(squareEveryDigit(6321510));
