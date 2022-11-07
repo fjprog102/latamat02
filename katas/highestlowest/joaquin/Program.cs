d002dd9299cc754a36ea3c46c935272cf9ef7930
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-11-07 16:48:38 -0300

    Add highestlowest kata resolution for Joaquin

---
+++ katas/highestlowest/joaquin/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Write input of space separated numbers:");
+var input = Console.ReadLine();
+
+Console.WriteLine(highAndLow(input));
+
+string highAndLow(string input) {
+
+    if (!input.Contains(' ')) return $"{input} {input}";
+    
+    string[] inputStringArray = input.Split(' ');
+    int[] inputIntegersArray = new int [inputStringArray.Length];
+
+    for (int i = 0; i<inputStringArray.Length; i++) {
+        inputIntegersArray[i] = int.Parse(inputStringArray[i]);
+    }
+    Array.Sort(inputIntegersArray);
+  
+    return $"{inputIntegersArray[inputIntegersArray.Length - 1]} {inputIntegersArray[0]}";
+}
