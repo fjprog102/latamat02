a0c748d426bf5c3db72b6d3b393f9c84216c10b3
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-08 17:45:36 -0300

    Added more unit test cases

--- katas/highestlowestTest/adriel/HighestLowestTest.cs
+++ katas/highestlowestTest/adriel/HighestLowestTest.cs
@@ -6,5 +6,9 @@ public class HighestLowestTest {
     [Fact]
     void ItShouldReturnTheHighestAndLowestNumbersOnAString() {
         Assert.Equal("5 1", new HighestAndLowest().getHighestAndLowest("1 2 3 4 5"));
+        Assert.Equal("5 -1", new HighestAndLowest().getHighestAndLowest("-1 2 3 4 5"));
+        Assert.Equal("64 -58", new HighestAndLowest().getHighestAndLowest("5 5 64 -58 4"));
+        Assert.Equal("100 10", new HighestAndLowest().getHighestAndLowest("100 50 10 20"));
+        Assert.Equal("45 -741", new HighestAndLowest().getHighestAndLowest("12 -54 0 45 -741"));
     }
 }
