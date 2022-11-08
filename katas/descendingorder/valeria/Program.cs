182d2b9ad87d6af311890264187ce9b8a8deb8bb
Beyta Gonzales <svaleria.gonzales@gmail.com>
2022-11-08 18:07:42 -0400

    change the namespace name and the class name

--- katas/descendingorder/valeria/Program.cs
+++ katas/descendingorder/valeria/Program.cs
@@ -1,30 +1,18 @@
 ﻿
 using System;
 using System.Linq;
-using System.Text.RegularExpressions;
 
-namespace MyConsoleApp
+namespace DescendingOrder
 {
-    public class Class1
+    public class DescendingOrder
     {
-        public static string DescendingOrder(int MyNumber)
+        public static string GetTheHighestValue(int MyNumber)
         {
             string res = "your input must be a non-negative integer";
             if(MyNumber > 0){
                 res = String.Concat(MyNumber.ToString().OrderByDescending(c => c));
             }
             return res;
-        }
-    }
-    public class Program
-    {
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine(Class1.DescendingOrder(42145));
-            Console.WriteLine(Class1.DescendingOrder(145263));
-            Console.WriteLine(Class1.DescendingOrder(123456789));
-            Console.WriteLine(Class1.DescendingOrder(-123456789));
         }
     }
 }
