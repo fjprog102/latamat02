9fb9f0360207da2f35c2952b4cd7f8f328c5e574
Jose Guevara <Jose.Guevara@Jala.University>
2022-11-08 01:15:36 -0400

    Refactor HelloWorld kata so the solutions use namespaces instead of being separated projects

--- katas/helloworld/Program.cs
+++ katas/helloworld/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(new HelloWorld.Adriel.Hello().SayHello());
+Console.WriteLine(new HelloWorld.Joaquin.Hello().SayHello());
+Console.WriteLine(new HelloWorld.Juan.Hello().SayHello());
+Console.WriteLine(new HelloWorld.Valeria.Hello().SayHello());
--- katas/helloworld/joaquin/Program.cs
+++ katas/helloworld/joaquin/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld.Joaquin;
+
+public class Hello {
+    public string SayHello() {
+        return "Hello, Joaquin!";
+    }
+}
