1ea01cd570c045966930c1b8ae08f16b0ca52f03
Schlvf <axlzbot@gmail.com>
2022-11-08 12:31:56 -0500

    Removed .csproj, warpped solution with namespace and class and added unit test for kata square-digits Juan

---
+++ katas/squaredigit/juan/SquareDigit.cs
@@ -0,0 +1,24 @@
+﻿namespace SquareDigit.Juan
+{
+    public class SquareDigitJuan
+    {
+        static public String square_digit(String user_input)
+        {
+            char[] values = user_input.ToArray();
+            String opt = "";
+
+            foreach (char val in user_input)
+            {
+                opt += get_square(val);
+            }
+            return opt;
+        }
+
+        static public String get_square(char value)
+        {
+            int parsed = Int32.Parse(value.ToString());
+            int square = parsed * parsed;
+            return square.ToString();
+        }
+    }
+}
