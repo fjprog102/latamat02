7744e9d7364458a38349403a53cf32fa06ba5785
Jorge Chavez <ljorchavez@gmail.com>
2022-11-08 12:28:21 -0600

    Wrapped program into a namespace and added a test to Descendingorder app

---
+++ katas/descendingorder/jorge/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DescendingOrder.Jorge;
+public class DescendingOrder{
+    public int Number(int num) {
+        char[] numArray = num.ToString().ToCharArray();
+        Array.Sort(numArray);
+        Array.Reverse(numArray);
+        var sortedNumber = new string(numArray);
+        return int.Parse(sortedNumber);
+    }
+}
+
+
+//char[] numberArray = number.ToCharArray();
+//Array.Sort(numberArray);
+//Array.Reverse(numberArray);
+//var sortedNumber = new string(numberArray);
+//Console.WriteLine("Your highest possible number is: " + sortedNumber);
