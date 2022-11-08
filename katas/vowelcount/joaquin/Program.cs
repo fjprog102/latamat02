59413f900b9a4711744385e1011e985c4d66524d
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-11-08 15:19:42 -0300

    Add VowelCountTest case and delete joaquin.csproj

--- katas/vowelcount/joaquin/Program.cs
+++ katas/vowelcount/joaquin/Program.cs
@@ -1,26 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Write your text here:");
-var input = Console.ReadLine();
+namespace VowelCount.Joaquin {
+    public class VowelCount {
+        public int vowelCount(string input) {
+            int count = 0;
+            char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+            
+            foreach (char ch in input) {
+                if (vowels.Contains(ch)) count ++;
+            }
 
-Console.WriteLine(vowelCount(input));
-
-string vowelCount(string input) {
-    int count = 0;
-    char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-    
-    for (int i = 0; i < input.Length; i++) {
-
-        if (input[i] == ' ') continue;
-        if (Char.IsUpper(input[i]) || Char.IsDigit(input[i]) || !char.IsLetterOrDigit(input[i])) {
-            i=0;
-            count=0;
-            Console.WriteLine("Please enter only lower case letters and/or spaces:");
-            input = Console.ReadLine();
-        };
-        if (vowels.Contains(input[i])) count ++;
+            return count;
+        }
     }
-
-    return $"The amount of vowels is {count}";
-
-};
-
+}
