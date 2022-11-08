a8333e90d577b895eb27af3d7bbca4b127949f6f
Jorge Chavez <ljorchavez@gmail.com>
2022-11-08 12:48:14 -0600

    Wrapped program into namespace and added a testcase

---
+++ katas/vowelcount/jorge/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VowelCount.Jorge;
+using System.Text.RegularExpressions;
+public class VowelCount{
+    public int Phrase(string text) {
+        var vowelCount = Regex.Replace(text, @"[^aeiouAEIOU]", "");
+        return vowelCount.Length;
+    }
+}
+
+
