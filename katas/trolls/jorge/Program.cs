6c1d6fc17d9927ddad7fa0f14c4e9e180091ef17
Jorge Chavez <ljorchavez@gmail.com>
2022-11-08 11:06:53 -0600

    Deleted local cdproj and wrapped program into namespace

---
+++ katas/trolls/jorge/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Trolls.Jorge;
+using System.Text.RegularExpressions;
+public class Trolls{
+    public string IsTrolls(string text) {
+        return Regex.Replace(text, @"[aeiouAEIOUáéíóúÁÉÍÓÚ]", "");
+    }
+}
