173b21a2f792c6f0044d66ae23302608d8a5a497
Beyta Gonzales <svaleria.gonzales@gmail.com>
2022-11-07 21:33:19 -0400

    Add trolls kata resolution for Valeria

---
+++ katas/trolls/valeria/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MyConsoleApp
+{
+    public class Troll
+    {
+        public static string EatVowels(string Mssg)
+        {
+            var Vowels = new HashSet<char>("aeiouAEIOU");
+            Mssg = new string(Mssg.Where(c => !Vowels.Contains(c)).ToArray());
+            return Mssg;
+        }
+    }
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Troll.EatVowels("This website is for losers LOL!"));
+        }
+    }
+}
