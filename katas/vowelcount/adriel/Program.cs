529565e008824c0d0a229a3ceea6651be7c45270
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-08 18:13:21 -0300

    Converted Vowel Count kata function into class, and created unit tests for Adriel

--- katas/vowelcount/adriel/Program.cs
+++ katas/vowelcount/adriel/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿namespace VowelCount.Adriel;
 using System.Text.RegularExpressions;
 
-int vowelCount(string s)
-{
-    Regex rx = new Regex("[a|e|i|o|u]", RegexOptions.IgnoreCase);
-     return rx.Matches(s).Count;
+public class VowelCounter {
+    public int vowelCount(string s)
+    {
+        Regex rx = new Regex("[aeiou]", RegexOptions.IgnoreCase);
+        return rx.Matches(s).Count;
+    }
 }
-
-System.Console.WriteLine(vowelCount("aaaajjj"));
-System.Console.WriteLine(vowelCount("akfjuehdgyua"));
-System.Console.WriteLine(vowelCount("This is a test for the function"));
