a836a9eab02a4e6fd39201d9015890158b2ae4ba
Beyta Gonzales <svaleria.gonzales@gmail.com>
2022-11-08 20:26:53 -0400

    trolls kata unit test done

--- katas/trolls/valeria/Program.cs
+++ katas/trolls/valeria/Program.cs
@@ -2,11 +2,11 @@
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace Troll
+namespace Trolls.Valeria
 {
     public class Troll
     {
-        public static string EatVowels(string Mssg)
+        public string EatVowels(string Mssg)
         {
             var Vowels = new HashSet<char>("aeiouAEIOU");
             Mssg = new string(Mssg.Where(c => !Vowels.Contains(c)).ToArray());
---
+++ katas/trollsTest/valeria/TrollsTest.cs
@@ -0,0 +1,18 @@
+using Trolls.Valeria;
+namespace Trolls.Test.Valeria
+{
+    public class TrollsTest
+    {
+        [Fact]
+        void ItShouldGiveMeTheMessageWithoutVowels()
+        {
+            Assert.Equal("Ths wbst s fr lsrs LL!", new Troll().EatVowels("This website is for losers LOL!"));
+            Assert.Equal(" hv 19875213 dllrs!! $.$", new Troll().EatVowels("I have 19875213 dollars!! $.$"));
+            Assert.Equal("Th Trll S GNN t M !#$^&*", new Troll().EatVowels("The TrOll IS GoNNA eAt MEEEE !#$^&*"));
+            Assert.Equal("7987521654", new Troll().EatVowels("7987521654"));
+            Assert.Equal("", new Troll().EatVowels("euAIEOoUaUai"));
+            Assert.Equal("", new Troll().EatVowels(""));
+            Assert.Equal(" ", new Troll().EatVowels(" "));
+        }
+    }
+}
