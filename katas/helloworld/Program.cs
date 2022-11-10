75599ba2ad204e9078f2133dd347f3a8b45d28b0
Jose Guevara <Jose.Guevara@Jala.University>
2022-11-10 02:48:32 -0400

    Delete project files from subfolders

--- katas/helloworld/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.WriteLine(new HelloWorld.Adriel.Hello().SayHello());
-Console.WriteLine(new HelloWorld.Joaquin.Hello().SayHello());
-Console.WriteLine(new HelloWorld.Juan.Hello().SayHello());
-Console.WriteLine(new HelloWorld.Valeria.Hello().SayHello());
