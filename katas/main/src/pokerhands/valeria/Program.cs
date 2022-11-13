9819d4ca067f8a5145783da8661d550c80c8b0f4
Beyta Gonzales <svaleria.gonzales@gmail.com>
2022-11-13 02:52:00 -0400

    solving format warning issues

--- katas/main/src/pokerhands/valeria/Program.cs
+++ katas/main/src/pokerhands/valeria/Program.cs
@@ -4,9 +4,8 @@ namespace PokerHands.Valeria
 {
     public class Program
     {
-        public static void Main(String[] args)
+        public static void Main()
         {
-            List<int> Values = new List<int>();
             GamePlayRules Rules = new GamePlayRules(new string[] {"AH","AC","AS","AD","3S"});
             string res = Rules.ApplyFourOfAKindRule();
             Console.WriteLine(res);
