508c41b5056e09737448c1140e5a1deb4d2f9a37
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-11-14 22:48:57 -0300

    fixed formatting issues

--- katas/main/src/pokerhands/joaquin/Game.cs
+++ katas/main/src/pokerhands/joaquin/Game.cs
@@ -15,22 +15,22 @@ public class Game
         firstPlayer.HandRanking.GetRankingType(firstPlayer.HandRanking.ranking);
         secondPlayer.HandRanking.GetHandRanking(secondPlayer.Hand);
         secondPlayer.HandRanking.GetRankingType(secondPlayer.HandRanking.ranking);
-        if(firstPlayer.HandRanking.ranking > secondPlayer.HandRanking.ranking) return $"{firstPlayer.name} wins with {firstPlayer.HandRanking.type}.";
-        else if(firstPlayer.HandRanking.ranking < secondPlayer.HandRanking.ranking) return $"{secondPlayer.name} wins with {secondPlayer.HandRanking.type}.";
+        if (firstPlayer.HandRanking.ranking > secondPlayer.HandRanking.ranking) return $"{firstPlayer.name} wins with {firstPlayer.HandRanking.type}.";
+        else if (firstPlayer.HandRanking.ranking < secondPlayer.HandRanking.ranking) return $"{secondPlayer.name} wins with {secondPlayer.HandRanking.type}.";
         else return EvaluateTie(firstPlayer, secondPlayer);
     }
 
     public string EvaluateTie(Player firstPlayer, Player secondPlayer)
     {
-        var orderedHandFirstPlayer = firstPlayer.HandRanking.OrderHandByCardWeight(firstPlayer.Hand);   
-        var orderedHandSecondPlayer = secondPlayer.HandRanking.OrderHandByCardWeight(secondPlayer.Hand); 
-        for(int i = 0; i < orderedHandFirstPlayer.Length; i++)
+        var orderedHandFirstPlayer = firstPlayer.HandRanking.OrderHandByCardWeight(firstPlayer.Hand);
+        var orderedHandSecondPlayer = secondPlayer.HandRanking.OrderHandByCardWeight(secondPlayer.Hand);
+        for (int i = 0; i < orderedHandFirstPlayer.Length; i++)
         {
-            if(orderedHandFirstPlayer[i] > orderedHandSecondPlayer[i]) return $"{firstPlayer.name} wins with high card.";
-            else if(orderedHandFirstPlayer[i] < orderedHandSecondPlayer[i]) return $"{secondPlayer.name} wins with high card.";
-        }  
+            if (orderedHandFirstPlayer[i] > orderedHandSecondPlayer[i]) return $"{firstPlayer.name} wins with high card.";
+            else if (orderedHandFirstPlayer[i] < orderedHandSecondPlayer[i]) return $"{secondPlayer.name} wins with high card.";
+        }
         return "Tie.";
     }
 
-    
+
 }
