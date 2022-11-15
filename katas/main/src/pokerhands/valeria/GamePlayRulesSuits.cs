10ea0ab43f266195108eed5ac5d65eff99a6ec00
Beyta Gonzales <svaleria.gonzales@gmail.com>
2022-11-15 03:14:39 -0400

    Naming rule violation solved in game play rules suits

--- katas/main/src/pokerhands/valeria/GamePlayRulesSuits.cs
+++ katas/main/src/pokerhands/valeria/GamePlayRulesSuits.cs
@@ -7,8 +7,8 @@ namespace PokerHands.Valeria
         public List<List<string>> ConsecutiveValues = new List<List<string>>(){new List<string>{"2", "3", "4", "5", "A"}, new List<string>{ "2", "3", "4", "5", "6"}, new List<string>{ "3", "4", "5", "6", "7"}, new List<string>{ "4", "5", "6", "7", "8"}, new List<string>{ "5", "6", "7", "8", "9"}, new List<string>{ "6", "7", "8", "9", "10"}, new List<string>{ "10", "7", "8", "9", "J"}, new List<string>{ "10", "8", "9", "J", "Q"},new List<string>{ "10", "9", "J", "K", "Q"},new List<string>{ "10", "A", "J", "K", "Q"}, new List<string>{ "2", "A", "J", "K", "Q"}, new List<string>{ "2", "3", "A", "K", "Q"}, new List<string>{ "2", "3", "4", "A", "K"}};
         public string ApplyStraightFlushRule()
         {
-            var ConsecutiveHand = from ListValues in ConsecutiveValues where ListValues.SequenceEqual(Values) select ListValues.ToList();
-            return SuitsCount.ContainsValue(5) && ConsecutiveHand.Count() > 0? "with straight flush: " + SuitsCount.ElementAt(0).Key : "None";
+            var consecutiveHand = from ListValues in ConsecutiveValues where ListValues.SequenceEqual(Values) select ListValues.ToList();
+            return SuitsCount.ContainsValue(5) && consecutiveHand.Count() > 0? "with straight flush: " + SuitsCount.ElementAt(0).Key : "None";
         }
         public string ApplyFlushRule()
         {
@@ -16,8 +16,8 @@ namespace PokerHands.Valeria
         }
         public string ApplyStraightRule()
         {
-            var ConsecutiveHand = from ListValues in ConsecutiveValues where ListValues.SequenceEqual(Values) select ListValues.ToList();
-            return ConsecutiveHand.Count() > 0 ? "with straight" : "None";
+            var consecutiveHand = from ListValues in ConsecutiveValues where ListValues.SequenceEqual(Values) select ListValues.ToList();
+            return consecutiveHand.Count() > 0 ? "with straight" : "None";
         }
         public string ApplyHighCardRule()
         {
