7d976501f116d31a18c825f9b08b411dd630c043
Schlvf <axlzbot@gmail.com>
2022-11-17 13:15:32 -0500

    Added Action class factory and Action class factory tests

---
+++ Game2048/GameTest/Models/Actions/MoveDownTests.cs
@@ -0,0 +1,15 @@
+﻿using AbstractActionClass;
+using ActionClass;
+
+namespace ActionsTests
+{
+    public class MoveDownTest
+    {
+        [Fact]
+        public void TestMoveDownIsChildInstance()
+        {
+            var instance = new MoveDown();
+            Assert.True(instance.Instance.GetType().IsSubclassOf(typeof(AbstractAction<MoveDown>)));
+        }
+    }
+}
