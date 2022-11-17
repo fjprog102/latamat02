cfd462d6292c7125c94a5b078f5b8865832ec125
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-17 15:11:05 -0300

    Added Tile class with Value and BackgroundColor properties and test cases

---
+++ Game2048/Game/Models/TileComponent.cs
@@ -0,0 +1,24 @@
+namespace Game2048.Models;
+
+// public class Tile : GridElement
+public class Tile
+{
+    public int Value { get; set; }
+    public string BackgroundColor
+    {
+        get
+        {
+            if (Value <= 1) return "#fff";
+            else if (Value <= 3) return "#aaa";
+            else if (Value <= 5) return "#aa0";
+            return "#a00";
+        }
+    }
+    
+    public Tile(int value)
+    {
+        if (value % 2 != 0) throw new ArgumentException("Tile value must be a multiple of two");
+
+        Value = value;
+    }
+}
