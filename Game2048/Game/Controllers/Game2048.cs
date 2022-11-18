53709594ac060a6f6df5be27649fc94cad9b7481
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-11-18 17:29:54 -0300

    Create Game abstract class; Game2048 class; conect with UI

---
+++ Game2048/Game/Controllers/Game2048.cs
@@ -0,0 +1,22 @@
+﻿namespace Controllers;
+
+using Models;
+
+public class Game2048 : Game
+{
+    public Grid? grid;
+
+    public new Score? score;
+
+    public override void Start()
+    {
+        grid = new Grid(4, 4);
+        score = new Score();
+    }
+
+    public Game2048()
+    {
+        Start();
+    }
+}
+
--- Game2048/Game/Models/Abstracts/GridElement.cs
+++ Game2048/Game/Models/Abstracts/GridElement.cs
@@ -2,7 +2,7 @@
 
 public abstract class GridElement
 {
-    private int Value { get; set; }
+    public int Value { get; set; }
     private string? BackgroundColor { get; set; }
     private string? NumberColor { get; set; }
 }
