e4d5afc03994893776d983546e0b16093a77097a
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-18 13:17:03 -0300

    Changed 'TileComponent' file to 'Tile', separated 'GetBackgroundColor' and 'GetNumberColor' into a separated class, and wrote test cases for them

---
+++ Game2048/Game/Models/Tile.cs
@@ -0,0 +1,21 @@
+﻿namespace Models;
+
+public class Tile : GridElement
+{
+    private readonly TileColors _tileColors = new TileColors();
+    public int Value { get; set; }
+    public string? BackgroundColor;
+    public string? NumberColor;
+
+    public Tile(int value)
+    {
+        if (value % 2 != 0)
+        {
+            throw new ArgumentException("Tile value must be a multiple of two");
+        }
+
+        Value = value;
+        BackgroundColor = _tileColors.GetBackgroundColor(value);
+        NumberColor = _tileColors.GetNumberColor(value);
+    }
+}
