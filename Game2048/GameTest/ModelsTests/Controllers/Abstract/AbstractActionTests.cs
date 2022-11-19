4d6ccae16afbb634d14d5987fb175b3c37874049
Schlvf <axlzbot@gmail.com>
2022-11-19 17:02:22 -0500

    Added actions functionalily on game grid

--- Game2048/GameTest/ModelsTests/Controllers/Abstract/AbstractActionTests.cs
+++ Game2048/GameTest/ModelsTests/Controllers/Abstract/AbstractActionTests.cs
@@ -3,6 +3,66 @@ using Models;
 
 namespace ActionsTests
 {
+    public static class ActionTestHelper
+    {
+        public static bool AreEqualGrids(Grid input, Grid expected)
+        {
+            for (int i = 0; i < input.Rows; i++)
+            {
+                for (int j = 0; i < input.Columns; i++)
+                {
+                    if (!AreEqualTile(input, expected, i, j))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static bool AreEqualTile(Grid input, Grid expected, int indexA, int indexB)
+        {
+            if (expected.Cells[indexA, indexB] == null)
+            {
+                if (input.Cells[indexA, indexB] != null)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (input.Cells[indexA, indexB] == null)
+                {
+                    return false;
+                }
+
+                if (expected.Cells[indexA, indexB]!.Value != input.Cells[indexA, indexB]!.Value)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool AreEqualLists(List<Tile?> input, List<Tile?> expected)
+        {
+            for (int i = 0; i < expected.Count(); i++)
+            {
+                if (expected.ElementAt(i) != null)
+                {
+                    if (input.ElementAt(i)!.Value != expected.ElementAt(i)!.Value)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
     public class AbstractActionTest
     {
         public Tile[] mockList = new Tile[4];
@@ -32,12 +92,12 @@ namespace ActionsTests
         }
 
         [Fact]
-        public void TestIsMergeRow()
+        public void TestIsMergeList()
         {
             var action = new MoveDown();
             List<Tile?> values = new List<Tile?> { null, new Tile(2), new Tile(2), new Tile(2) };
 
-            values = action.MergeRow(values);
+            values = action.MergeList(values);
 
             List<Tile?> expected = new List<Tile?> { null, new Tile(2), null, new Tile(4) };
 
@@ -51,12 +111,12 @@ namespace ActionsTests
         }
 
         [Fact]
-        public void TestMoveRow()
+        public void TestMoveList()
         {
             var action = new MoveDown();
             List<Tile?> values = new List<Tile?> { null, new Tile(2), new Tile(4), null };
 
-            values = action.MoveRow(values);
+            values = action.MoveList(values);
 
             List<Tile?> expected = new List<Tile?> { null, null, new Tile(2), new Tile(4) };
 
@@ -67,6 +127,84 @@ namespace ActionsTests
                     Assert.Equal(values.ElementAt(i)!.Value, expected.ElementAt(i)!.Value);
                 }
             }
+        }
+
+        [Fact]
+        public void TestGetRow()
+        {
+            var gridMock = new Grid(2, 2);
+
+            gridMock.InsertElement(0, 0, new Tile(2));
+            gridMock.InsertElement(0, 1, new Tile(4));
+            gridMock.InsertElement(1, 0, new Tile(8));
+            gridMock.InsertElement(1, 1, new Tile(16));
+
+            var action = new MoveDown();
+            var row = action.Instance.GetRow(gridMock, 0).ToList();
+            var expected = new List<Tile?> { new Tile(2), new Tile(4) };
+
+            var row2 = action.Instance.GetRow(gridMock, 1).ToList();
+            var expected2 = new List<Tile?> { new Tile(8), new Tile(16) };
+
+            Assert.True(ActionTestHelper.AreEqualLists(row, expected));
+            Assert.True(ActionTestHelper.AreEqualLists(row2, expected2));
+        }
+
+        [Fact]
+        public void TestGetColumn()
+        {
+            var gridMock = new Grid(2, 2);
+
+            gridMock.InsertElement(0, 0, new Tile(2));
+            gridMock.InsertElement(0, 1, new Tile(4));
+            gridMock.InsertElement(1, 0, new Tile(8));
+            gridMock.InsertElement(1, 1, new Tile(16));
+
+            var action = new MoveDown();
+            var row = action.Instance.GetColumn(gridMock, 0).ToList();
+            var expected = new List<Tile?> { new Tile(2), new Tile(8) };
+
+            var row2 = action.Instance.GetColumn(gridMock, 1).ToList();
+            var expected2 = new List<Tile?> { new Tile(4), new Tile(16) };
+
+            Assert.True(ActionTestHelper.AreEqualLists(row, expected));
+            Assert.True(ActionTestHelper.AreEqualLists(row2, expected2));
+        }
+
+        [Fact]
+        public void TestSetRow()
+        {
+            var gridMock = new Grid(2, 2);
+
+            var action = new MoveDown();
+            var row = new List<Tile?> { new Tile(2), new Tile(4) };
+
+            action.Instance.SetRow(gridMock, row, 0);
+
+            var expected = new Grid(2, 2);
+
+            expected.InsertElement(0, 0, new Tile(2));
+            expected.InsertElement(0, 1, new Tile(4));
+
+            Assert.True(ActionTestHelper.AreEqualGrids(gridMock, expected));
+        }
+
+        [Fact]
+        public void TestSetColumn()
+        {
+            var gridMock = new Grid(2, 2);
+
+            var action = new MoveDown();
+            var column = new List<Tile?> { new Tile(2), new Tile(4) };
+
+            action.Instance.SetColumn(gridMock, column, 0);
+
+            var expected = new Grid(2, 2);
+
+            expected.InsertElement(0, 0, new Tile(2));
+            expected.InsertElement(1, 0, new Tile(4));
+
+            Assert.True(ActionTestHelper.AreEqualGrids(gridMock, expected));
         }
     }
 }
