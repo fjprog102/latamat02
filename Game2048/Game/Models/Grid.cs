6b5f7252815b73623c2ab9a561ece00af475b269
Beyta Gonzales <svaleria.gonzales@gmail.com>
2022-11-20 19:35:26 -0400

    in generate random tile function, a new value is generated for a tile

--- Game2048/Game/Models/Grid.cs
+++ Game2048/Game/Models/Grid.cs
@@ -36,6 +36,7 @@ public class Grid
             }
         }
     }
+
     public void InsertElement(int x, int y, Tile element)
     {
         if (x > Cells.GetLength(0) || y > Cells.GetLength(1))
@@ -45,11 +46,14 @@ public class Grid
 
         Cells[x, y] = element;
     }
+
     public void GenerateRandomTile(Tile element)
     {
         VerifyEmptyCells();
         Random random = new Random();
         int randomPosition = random.Next(EmptyCells.Count);
+        int[] TileValues = { 2, 4 };
+        element.Value = TileValues[random.Next(TileValues.Length)];
         InsertElement(EmptyCells[randomPosition][0], EmptyCells[randomPosition][1], element);
         VerifyEmptyCells();
     }
