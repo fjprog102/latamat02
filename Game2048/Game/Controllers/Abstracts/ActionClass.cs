4bd86f9d14541bd1fba65bafab5481c6912cb237
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-11-20 21:45:28 -0300

    Connect movement actions with Game on UI; create restart and UpdateBestScore methods for Game

--- Game2048/Game/Controllers/Abstracts/ActionClass.cs
+++ Game2048/Game/Controllers/Abstracts/ActionClass.cs
@@ -22,6 +22,7 @@ namespace Actions
                             values[i]!.IncreaseValue();
                             break;
                         }
+                        break;
                     }
                 }
             }
