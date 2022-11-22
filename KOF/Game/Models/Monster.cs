9f1a339e26e2958004db64c420dd1c022478b7db
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-22 16:04:28 -0300

    Fixed nullable warning

--- KOF/Game/Models/Monster.cs
+++ KOF/Game/Models/Monster.cs
@@ -3,7 +3,7 @@
 public class Monster
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public int VictoryPoints { get; set; }
     public int LifePoints { get; set; }
 }
