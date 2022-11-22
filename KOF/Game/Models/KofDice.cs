4c6ef64d584a3584e85ee6b91bb7c3a0ad20103d
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-11-22 18:43:40 -0300

    Created Dice abstract class; KofDice Class with initial attributes, methods and unit tests

---
+++ KOF/Game/Models/KofDice.cs
@@ -0,0 +1,43 @@
+﻿namespace Models;
+
+using Models.Abstracts;
+
+public class KofDice : Dice
+{
+    public List<string>[] BlackDices = new List<string>[6];
+    public List<string>[] GreenDices = new List<string>[2];
+
+    public new string[] Symbols = { "one", "two", "three", "heart", "energy", "smash" };
+
+    public KofDice()
+    {
+        foreach (string symbol in Symbols)
+        {
+            Faces.Add(symbol);
+        }
+
+        GenerateKofDices(BlackDices);
+        GenerateKofDices(GreenDices);
+    }
+
+    public void GenerateKofDices(List<string>[] dices)
+    {
+        for (int index = 0; index < dices.Length; index++)
+        {
+            dices[index] = Faces;
+        }
+    }
+
+    public List<string> RollDices(List<string>[] dices)
+    {
+        List<string> rolledDices = new List<string>();
+        Random random = new Random();
+
+        for (int index = 0; index < dices.Length; index++)
+        {
+            rolledDices.Add(dices[index][random.Next(dices[index].Count() - 1)]);
+        }
+
+        return rolledDices;
+    }
+}
---
+++ KOF/GameTest/ModelsTest/KofDiceTest.cs
@@ -0,0 +1,56 @@
+﻿namespace ModelsTests;
+
+using Models;
+
+public class KofDiceTest
+{
+    private readonly KofDice kofDice = new KofDice();
+
+    [Fact]
+    public void AKofDiceShouldHaveSixFaces()
+    {
+        Assert.Equal(6, kofDice.Faces.Count());
+    }
+
+    [Fact]
+    public void AKofDiceFaceShouldHaveAValidSymbol()
+    {
+        foreach (string face in kofDice.Faces)
+        {
+            Assert.Contains(face, kofDice.Symbols);
+        }
+    }
+
+    [Fact]
+    public void ItShouldHaveSixBlackDicesWithSixFaces()
+    {
+        Assert.Equal(6, kofDice.BlackDices.Length);
+
+        foreach (List<string> dice in kofDice.BlackDices)
+        {
+            Assert.Equal(dice, kofDice.Faces);
+        }
+    }
+
+    [Fact]
+    public void ItShouldHaveTwoGreenDicesWithSixFaces()
+    {
+        Assert.Equal(2, kofDice.GreenDices.Length);
+        foreach (List<string> dice in kofDice.GreenDices)
+        {
+            Assert.Equal(dice, kofDice.Faces);
+        }
+    }
+
+    [Fact]
+    public void ItShouldRollDicesAndReturnAListWithSixRandomFaces()
+    {
+        var rolledDices = kofDice.RollDices(kofDice.BlackDices);
+        Assert.IsType<List<string>>(rolledDices);
+
+        foreach (string face in rolledDices)
+        {
+            Assert.Contains(face, kofDice.Symbols);
+        }
+    }
+}
