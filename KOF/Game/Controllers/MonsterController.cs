55c7caa610acbc046f12c18918be13c08151f1a8
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-22 22:46:46 -0300

    Added Monster controller and test cases

---
+++ KOF/Game/Controllers/MonsterController.cs
@@ -0,0 +1,26 @@
+namespace KOF.Controllers;
+
+using KOF.Models;
+using KOF.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+[ApiController]
+[Route("[controller]")]
+public class MonsterController : ControllerBase
+{
+    public MonsterController()
+    {
+    }
+
+    [HttpGet("GetAll")]
+    public ActionResult<List<Monster>> GetAll() =>
+        MonsterService.GetAll();
+
+    [HttpPost]
+    public IActionResult Create(Monster monster)
+    {            
+        MonsterService.Add(monster);
+        return CreatedAtAction(nameof(Create), new { id = monster.Id }, monster);
+    }
+}
---
+++ KOF/GameTest/ControllersTest/MonsterControllerTest.cs
@@ -0,0 +1,24 @@
+﻿namespace KOF.Controllers.Test;
+
+using KOF.Controllers;
+using KOF.Models;
+using KOF.Services;
+using Microsoft.AspNetCore.Mvc;
+
+public class MonsterControllerTest
+{
+    private readonly MonsterController mc = new MonsterController();
+
+    [Fact]
+    public void ShouldReturnAnActionResultListOfMonsters()
+    {
+        Assert.IsType<ActionResult<List<Monster>>>(mc.GetAll());
+    }
+
+    [Fact]
+    public void ShouldReturnACreatedAtActionResult()
+    {
+        var monster = new Monster { Name = "New Monster", VictoryPoints = 10, LifePoints = 10 };
+        Assert.IsType<CreatedAtActionResult>(mc.Create(monster));
+    }
+}
