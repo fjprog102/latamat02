c25852e24276637890c14deba4b39fdc95536ce3
unknown <svaleria.gonzales@gmail.com>
2022-11-23 08:47:23 -0400

    warnings fixed in player model

--- KOF/Game/Models/Player.cs
+++ KOF/Game/Models/Player.cs
@@ -2,10 +2,10 @@
 {
     public class Player
     {
-        public string PID { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string DisplayName { get; set; }
-        public string PhotoID { get; set; }
+        public string? PID { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? DisplayName { get; set; }
+        public string? PhotoID { get; set; }
     }
 }
