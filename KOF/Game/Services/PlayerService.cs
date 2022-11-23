6e776b88b7006b61feb0d4618311706937c38ae3
unknown <svaleria.gonzales@gmail.com>
2022-11-23 08:40:39 -0400

    player service add method and constructor implemented

--- KOF/Game/Services/PlayerService.cs
+++ KOF/Game/Services/PlayerService.cs
@@ -4,45 +4,62 @@ using KOF.Models;
 
 public static class PlayerService
 {
-    public static class PizzaService
-{
     static List<Player> Players { get; }
-    //static int nextId = 3;
+
     static PlayerService()
     {
         Players = new List<Player>
         {
-            new Player { Id = "aodfjD5f", Name = "Classic Italian", IsGlutenFree = false },
-            new Player { Id = "2sdfXx6f", Name = "Veggie", IsGlutenFree = true }
+            new Player
+            {
+                PID = "aodfjD5f",
+                DisplayName = "Pedro",
+                Email = "pedro@gmail.com",
+                Password = "asd123",
+                PhotoID = "xkslf123ad5"
+            },
+            new Player
+            {
+                PID = "AsdldF$d",
+                DisplayName = "Pablo",
+                Email = "pablo@gmail.com",
+                Password = "asd1234",
+                PhotoID = "5sdf3xdf5gd"
+            }
         };
     }
 
     public static List<Player> GetAll() => Players;
 
-    public static Player? Get(int id) => Players.FirstOrDefault(p => p.Id == id);
+    public static Player? Get(string pid) => Players.FirstOrDefault(p => p.PID == pid);
 
     public static void Add(Player player)
     {
-        player.Id = nextId++;
+        const string chars =
+            "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@$^*()";
+        Random random = new Random();
+        var randomPID = new string(
+            Enumerable.Repeat(chars, 16).Select(s => s[random.Next(s.Length)]).ToArray()
+        );
+        player.PID = randomPID;
         Players.Add(player);
     }
 
-    public static void Delete(int id)
+    public static void Delete(string id)
     {
         var player = Get(id);
-        if(player is null)
+        if (player is null)
             return;
 
         Players.Remove(player);
     }
 
-    public static void Update(Players player)
+    public static void Update(Player player)
     {
-        var index = Players.FindIndex(p => p.Id == player.Id);
-        if(index == -1)
+        var index = Players.FindIndex(p => p.PID == player.PID);
+        if (index == -1)
             return;
 
         Players[index] = player;
     }
-}
 }
