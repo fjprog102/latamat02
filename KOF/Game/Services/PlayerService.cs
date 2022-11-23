8f09b373068f7471f044cff80d7ca752ead38042
unknown <svaleria.gonzales@gmail.com>
2022-11-23 14:14:53 -0400

    Player service and its unit test done

--- KOF/Game/Services/PlayerService.cs
+++ KOF/Game/Services/PlayerService.cs
@@ -1,64 +1,59 @@
-namespace KOF.Services;
+﻿namespace KOF.Services;
 
 using KOF.Models;
 
-public static class PlayerService
+public class PlayerService
 {
-    static List<Player> Players { get; }
+    private List<Player> Players { get; }
+    private int nextId = 3;
 
-    static PlayerService()
+    public PlayerService()
     {
         Players = new List<Player>
         {
             new Player
             {
-                PID = "aodfjD5f",
-                DisplayName = "Pedro",
-                Email = "pedro@gmail.com",
-                Password = "asd123",
-                PhotoID = "xkslf123ad5"
+                PID = 1,
+                Name = "Pedro",
+                MyMonster = new Monster()
             },
             new Player
             {
-                PID = "AsdldF$d",
-                DisplayName = "Pablo",
-                Email = "pablo@gmail.com",
-                Password = "asd1234",
-                PhotoID = "5sdf3xdf5gd"
+                PID = 2,
+                Name = "Pablo",
+                MyMonster = new Monster()
             }
         };
     }
 
-    public static List<Player> GetAll() => Players;
+    public List<Player> GetAll() => Players;
 
-    public static Player? Get(string pid) => Players.FirstOrDefault(p => p.PID == pid);
+    public Player? Get(int pid) => Players.FirstOrDefault(p => p.PID == pid);
 
-    public static void Add(Player player)
+    public void Add(Player player)
     {
-        const string chars =
-            "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@$^*()";
-        Random random = new Random();
-        var randomPID = new string(
-            Enumerable.Repeat(chars, 16).Select(s => s[random.Next(s.Length)]).ToArray()
-        );
-        player.PID = randomPID;
+        player.PID = nextId++;
         Players.Add(player);
     }
 
-    public static void Delete(string id)
+    public void Delete(int pid)
     {
-        var player = Get(id);
+        var player = Get(pid);
         if (player is null)
+        {
             return;
+        }
 
         Players.Remove(player);
     }
 
-    public static void Update(Player player)
+    public void Update(Player player)
     {
         var index = Players.FindIndex(p => p.PID == player.PID);
         if (index == -1)
+        {
             return;
+        }
 
         Players[index] = player;
     }
---
+++ KOF/GameTest/ServicesTest/PlayerServiceTest.cs
@@ -0,0 +1,61 @@
+﻿namespace KOF.Services.Test;
+
+using KOF.Models;
+using KOF.Services;
+
+public class PlayerServiceTest
+{
+    [Fact]
+    public void ShouldReturnAListWithAllPlayers()
+    {
+        PlayerService playerService = new PlayerService();
+        Assert.IsType<List<Player>>(playerService.GetAll());
+        Assert.True(playerService.GetAll().Any());
+    }
+
+    [Fact]
+    public void ShouldReturnAPlayerByPID()
+    {
+        PlayerService playerService = new PlayerService();
+        Assert.False(object.ReferenceEquals(null, playerService.Get(1)));
+        Assert.False(object.ReferenceEquals(null, playerService.Get(2)));
+    }
+
+    [Fact]
+    public void ShouldReturnNonNullObjectWhenANewPlayerIsCreated()
+    {
+        PlayerService playerService = new PlayerService();
+        playerService.Add(
+            new Player()
+            {
+                PID = 3,
+                Name = "Kive",
+                MyMonster = new Monster()
+            }
+        );
+        Assert.False(object.ReferenceEquals(null, playerService.Get(3)));
+    }
+
+    [Fact]
+    public void ShouldReturnNullWhenAPlayerIsDeleted()
+    {
+        PlayerService playerService = new PlayerService();
+        playerService.Delete(2);
+        Assert.True(object.ReferenceEquals(null, playerService.Get(2)));
+    }
+
+    [Fact]
+    public void ShouldReturnNewNameWhenAPlayerIsUpdated()
+    {
+        PlayerService playerService = new PlayerService();
+        Assert.Equal("Pablo", playerService.Get(2)?.Name);
+        Player player = new Player()
+        {
+            PID = 2,
+            Name = "Jose",
+            MyMonster = new Monster()
+        };
+        playerService.Update(player);
+        Assert.Equal("Jose", playerService.Get(2)?.Name);
+    }
+}
