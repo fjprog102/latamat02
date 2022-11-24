e6bd3b304f06cdc8fec1f025ecddd7cd5832544c
Adriel Amorim <agimenescomercial@gmail.com>
2022-11-24 10:19:21 -0300

    Fixed instance of service

--- KOF/Game/Controllers/MonsterController.cs
+++ KOF/Game/Controllers/MonsterController.cs
@@ -8,18 +8,19 @@ using Microsoft.AspNetCore.Mvc;
 [Route("[controller]")]
 public class MonsterController : ControllerBase
 {
+    private readonly MonsterService _service = new MonsterService();
     public MonsterController()
     {
     }
 
     [HttpGet("GetAll")]
     public ActionResult<List<Monster>> GetAll() =>
-        MonsterService.GetAll();
+        _service.GetAll();
 
     [HttpPost]
     public IActionResult Create(Monster monster)
     {
-        MonsterService.Add(monster);
+        _service.Add(monster);
         return CreatedAtAction(nameof(Create), new { id = monster.Id }, monster);
     }
 }
