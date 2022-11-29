b8257983bf384436feba64604000f8ed1d016bb9
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-11-29 14:37:45 -0300

    Refactor Game Controller and Game model, implement IGameService

--- KOF/Game/Controllers/Elements/GameControllers.cs
+++ KOF/Game/Controllers/Elements/GameControllers.cs
@@ -1,5 +1,7 @@
-﻿using KOT.Models;
-using KOT.Services;
+﻿using System.Text.Json;
+using KOT.Models;
+using KOT.Models.Abstracts;
+using KOT.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KOT.Controllers;
@@ -8,21 +10,82 @@ namespace KOT.Controllers;
 [Route("[controller]")]
 public class GameController : ControllerBase
 {
-    public GameController() { }
+    private readonly IGameService gameService;
 
-    // GET all action
-    [HttpGet]
-    public ActionResult<List<Game>> GetAll() => GameService.GetAll();
-
-    // POST action
-    [HttpPost]
-    public IActionResult Create(Game game)
+    public GameController(IGameService instance)
     {
-        GameService.Add(game);
-        return CreatedAtAction(nameof(Create), new { id = game.Id }, game);
+        gameService = instance;
     }
 
-    // PUT action
+    [HttpGet(Name = "GetName")]
+    public IActionResult GetAll([FromBody] GamePayload payload)
+    {
+        try
+        {
+            var result = gameService.Read(payload);
+            return Ok(result);
+        }
+        catch (Exception)
+        {
+            return BadRequest();
+        }
+    }
 
-    // DELETE action
+    [HttpGet("{id}")]
+    public IActionResult Get(string id)
+    {
+        try
+        {
+            var payload = new GamePayload(id);
+            var result = gameService.Read(payload);
+            return Ok(result);
+        }
+        catch (Exception)
+        {
+            return BadRequest();
+        }
+    }
+
+    [HttpPost(Name = "PostGame")]
+    public IActionResult Post([FromBody] GamePayload payload)
+    {
+        try
+        {
+            var result = gameService.Create(payload);
+            return Created("Tests", result.First());
+        }
+        catch (Exception)
+        {
+            return BadRequest();
+        }
+    }
+
+    [HttpPut(Name = "PutGame")]
+    public IActionResult Put([FromBody] GamePayload payload)
+    {
+        try
+        {
+            var result = gameService.Update(payload);
+            return Created("Tests", result.First());
+        }
+        catch (Exception)
+        {
+            return BadRequest();
+        }
+    }
+
+    [HttpDelete(Name = "DeleteGame")]
+    public IActionResult Delete([FromBody] GamePayload payload)
+    {
+        try
+        {
+            var result = gameService.Delete(payload);
+            return Ok(result);
+        }
+        catch (Exception)
+        {
+            return BadRequest();
+        }
+    }
 }
+
--- KOF/Game/Services/Elements/GameService.cs
+++ KOF/Game/Services/Elements/GameService.cs
@@ -1,46 +1,66 @@
 ﻿namespace KOT.Services;
 
 using KOT.Models;
+using KOT.Models.Abstracts;
+using KOT.Services.Interfaces;
 
-public static class GameService
+public class GameService : IGameService
 {
-    private static List<Game> Games { get; }
 
-    private static int nextId = 4;
+    private static readonly TokyoBoard Board = new TokyoBoard(new List<Player>() { new Player(), new Player() });
+    private readonly List<Game> Games = new List<Game> { new Game(Board) };
 
-    static GameService()
+    IEnumerable<Element> IGameService.Read(DataHolder payload)
     {
-        Games = new List<Game>
+        if (payload.Id != null)
         {
-            new Game
-            {
-                Id = 1,
-                TokyoBoard = "Tokyo City",
-                NumberOfPlayers = 3,
-                ActiveUser = 1
-            },
-            new Game
-            {
-                Id = 2,
-                TokyoBoard = "Tokyo Bay",
-                NumberOfPlayers = 6,
-                ActiveUser = 1
-            },
-            new Game
-            {
-                Id = 3,
-                TokyoBoard = "Tokyo Bay",
-                NumberOfPlayers = 2,
-                ActiveUser = 1
-            }
-        };
+            return Games.Where(game => game.Id!.Equals(payload.Id));
+        }
+
+        return Games;
     }
 
-    public static List<Game> GetAll() => Games;
-
-    public static void Add(Game game)
+    IEnumerable<Element> IGameService.Create(DataHolder payload)
     {
-        game.Id = nextId++;
-        Games.Add(game);
+        if (payload.GetType() == typeof(GamePayload))
+        {
+            GamePayload args = (GamePayload)payload;
+            var newGame = new Game((TokyoBoard)args.Board!);
+            Games.Add(newGame);
+            return new Element[] { newGame };
+        }
+
+        return new Element[0];
+    }
+
+    IEnumerable<Element> IGameService.Delete(DataHolder payload)
+    {
+        if (payload.Id != null)
+        {
+            var game = Games
+                .Where(game => game.Id!.Equals(payload.Id))
+                .ToArray();
+
+            Games.Remove(game.First());
+
+            return game;
+        }
+
+        return new Element[0];
+    }
+
+    IEnumerable<Element> IGameService.Update(DataHolder payload)
+    {
+        if (payload.Id != null || payload.GetType() == typeof(GamePayload))
+        {
+            GamePayload args = (GamePayload)payload;
+            var newGame = new Game((TokyoBoard)args.Board!);
+
+            Games[Games.FindIndex(game => game.Id == payload.Id)] = newGame;
+
+            return new Element[] { newGame };
+        }
+
+        return new Element[0];
     }
 }
