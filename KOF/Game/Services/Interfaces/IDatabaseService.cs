872fbeeddd24be5e4fcfae0fa305856c4d766b13
Sch <Schlvf>
2022-12-01 16:55:16 -0500

    Added mongo driver handler and powercard service tests

---
+++ KOF/Game/Services/Interfaces/IDatabaseService.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Driver;
+
+namespace KOT.Services.Interfaces;
+
+public interface IDatabaseService
+{
+    public FilterDefinition<T> GetFilterId<T>(string value);
+    public IEnumerable<T> Find<T>(string collectionName, FilterDefinition<T>? filter);
+    public void InsertOne<T>(string collectionName, T document);
+    public T Delete<T>(string collectionName, FilterDefinition<T> filter);
+}
--- KOF/GameTest/ServicesTest/Elements/PowerCardServiceTests.cs
+++ KOF/GameTest/ServicesTest/Elements/PowerCardServiceTests.cs
@@ -1,16 +1,9 @@
-﻿using System.Text.Json;
-using KOT.Controllers;
-using KOT.Models;
-using KOT.Models.Abstracts;
+﻿using KOT.Models;
 using KOT.Services;
-using KOT.Tests;
-using Microsoft.AspNetCore.Mvc;
+using KOT.Services.Interfaces;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using Moq;
-using Xunit.Abstractions;
-
-namespace KOT.Tests;
 
 public class PowerCardServiceMockData
 {
@@ -19,48 +12,189 @@ public class PowerCardServiceMockData
         return new List<PowerCard>
         {
             new PowerCard("name1", 1, 0),
-            new PowerCard("name2", 2, 1),
-            new PowerCard("name3", 3, 0),
+            new PowerCard("name2", 1, 0),
+            new PowerCard("name3", 1, 0),
         };
     }
 
     public static List<PowerCard>? GetSingleMock()
     {
-        return new List<PowerCard> { new PowerCard("name", 1, 1) };
+        return new List<PowerCard> { new PowerCard("name1", 1, 0) };
+    }
+
+    public static void DoNothing()
+    {
+        //Does nothing for mocking purposes
     }
 }
 
 public class PowerCardServiceTests
 {
-    /*
     [Fact]
-    public void TestReadMethod()
+    public void TestReadMethodWithId()
     {
-        var optionsMock = new DataBaseTestHelper("PowerCards");
-        var patched = new PowerCardService(optionsMock.GetOptions.Object);
+        //Set expected result
+        var expected = PowerCardServiceMockData.GetSingleMock();
+
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.PowerCardCollectionName = "PowerCards";
+        var options = Options.Create(settings);
+
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+        var filterMock = new Mock<FilterDefinition<PowerCard>>();
+
+        //Setup database service patches
+        dbMock.Setup(x => x.GetFilterId<PowerCard>(It.IsAny<string>())).Returns(filterMock.Object);
+        dbMock
+            .Setup(x => x.Find<PowerCard>(It.IsAny<string>(), filterMock.Object))
+            .Returns(expected!);
+
+        //Initialize service
+        var service = new PowerCardService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new PowerCardPayload(id: "Non null");
+
+        var result = service.Read(payload: payload);
+
+        Assert.Equal(typeof(List<PowerCard>), expected!.GetType());
+        Assert.Equal(expected: expected, actual: result);
+    }
+
+    [Fact]
+    public void TestReadMethodWithoutId()
+    {
+        //Set expected result
+        var expected = PowerCardServiceMockData.GetListMock();
+
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.PowerCardCollectionName = "PowerCards";
+        var options = Options.Create(settings);
+
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+        var filterMock = new Mock<FilterDefinition<PowerCard>>();
+
+        //Setup database service patches
+        dbMock.Setup(x => x.GetFilterId<PowerCard>(It.IsAny<string>())).Returns(filterMock.Object);
+        dbMock.Setup(x => x.Find<PowerCard>(It.IsAny<string>(), null)).Returns(expected!);
+
+        //Initialize service
+        var service = new PowerCardService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
         var payload = new PowerCardPayload();
-        var x = patched.Read(payload: payload);
 
-        output.WriteLine(x.Count().ToString());
-        Assert.True(false);
+        var result = service.Read(payload: payload);
+
+        Assert.Equal(typeof(List<PowerCard>), expected!.GetType());
+        Assert.Equal(expected: expected, actual: result);
     }
-    
 
     [Fact]
-    public void TestReadMethod()
+    public void TestCreateMethodExpectedElement()
     {
-        var mockIMongoCollection = new Mock<IMongoCollection<PowerCard>>();
-        var asyncCursor = new Mock<IAsyncCursor<PowerCard>>();
+        //Set expected result
+        var expected = PowerCardServiceMockData.GetSingleMock();
 
-        var expectedResult = PowerCardServiceMockData.GetListMock();
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.PowerCardCollectionName = "PowerCards";
+        var options = Options.Create(settings);
 
-        asyncCursor.SetupSequence(_async => _async.MoveNext(default)).Returns(true).Returns(false);
-        asyncCursor.SetupGet(_async => _async.Current).Returns(expectedResult!);
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
 
-        var service = PowerCardService()
-        var result = LoadPeople(mockIMongoCollection.Object);
+        //Setup database service patches
+        dbMock
+            .Setup(x => x.InsertOne<PowerCard>(It.IsAny<string>(), It.IsAny<PowerCard>()))
+            .Callback(PowerCardServiceMockData.DoNothing);
 
-        Assert.Equals(expectedResult, result);
+        //Initialize service
+        var service = new PowerCardService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new PowerCardPayload(name: "name1", cost: 1, type: 0);
+
+        var result = service.Create(payload: payload);
+
+        Assert.Equal(typeof(List<PowerCard>), expected!.GetType());
+        Assert.Single(collection: result);
     }
-    */
+
+    [Fact]
+    public void TestCreateMethodWrongElement()
+    {
+        //Set expected result
+        var expected = new List<PowerCard> { };
+
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.PowerCardCollectionName = "PowerCards";
+        var options = Options.Create(settings);
+
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+
+        //Initialize service
+        var service = new PowerCardService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new PlayerPayload();
+
+        var result = service.Create(payload: payload);
+
+        Assert.Equal(typeof(List<PowerCard>), expected!.GetType());
+        Assert.Empty(collection: result);
+    }
+
+    [Fact]
+    public void TestDeleteMethodWithId()
+    {
+        //Set expected result
+        var expected = PowerCardServiceMockData.GetSingleMock();
+
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.PowerCardCollectionName = "PowerCards";
+        var options = Options.Create(settings);
+
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+        var filterMock = new Mock<FilterDefinition<PowerCard>>();
+
+        //Setup database service patches
+        dbMock.Setup(x => x.GetFilterId<PowerCard>(It.IsAny<string>())).Returns(filterMock.Object);
+        dbMock
+            .Setup(x => x.Delete<PowerCard>(It.IsAny<string>(), filterMock.Object))
+            .Returns(expected!.First());
+
+        //Initialize service
+        var service = new PowerCardService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new PowerCardPayload(id: "Non null");
+
+        var result = service.Delete(payload: payload);
+
+        Assert.Equal(typeof(List<PowerCard>), expected!.GetType());
+        Assert.Equal(expected: expected, actual: result);
+    }
+
+    [Fact]
+    public void TestDeleteMethodWithoutId()
+    {
+        //Set expected result
+        var expected = new List<PowerCard> { };
+
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.PowerCardCollectionName = "PowerCards";
+        var options = Options.Create(settings);
+
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+
+        //Initialize service
+        var service = new PowerCardService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new PowerCardPayload();
+
+        var result = service.Delete(payload: payload);
+
+        Assert.Equal(typeof(List<PowerCard>), expected!.GetType());
+        Assert.Empty(collection: result);
+    }
 }
