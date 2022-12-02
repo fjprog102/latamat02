6988f76cd944e507221442f6274c313ef7464662
Adriel Amorim <agimenescomercial@gmail.com>
2022-12-02 17:33:47 -0300

    Added singleton Database service injection to MonsterService

--- KOF/Game/Services/Elements/MonsterService.cs
+++ KOF/Game/Services/Elements/MonsterService.cs
@@ -4,80 +4,63 @@ using KOT.Models;
 using KOT.Models.Abstracts;
 using KOT.Services.Interfaces;
 using Microsoft.Extensions.Options;
-using MongoDB.Driver;
 
 public class MonsterService : IMonsterService
 {
-    private readonly List<Monster> Monsters = new List<Monster>
-    {
-        new Monster("CyberKitty", 5, 10),
-        new Monster("Gigazaur", 7, 10)
-    };
+    private readonly string _monsterCollection;
+    private readonly IDatabaseService _dbInstance;
 
-    private readonly IMongoCollection<Monster> MonsterCollection;
-
-    public MonsterService(IOptions<KOTDatabaseSettings> kotDatabaseSettings)
+    public MonsterService(
+        IOptions<KOTDatabaseSettings> kotDatabaseSettings,
+        IDatabaseService dbInstance
+    )
     {
-        var mongoClient = new MongoClient(kotDatabaseSettings.Value.ConnectionString);
-        var mongoDatabase = mongoClient.GetDatabase(kotDatabaseSettings.Value.DatabaseName);
-        MonsterCollection = mongoDatabase.GetCollection<Monster>(
-            kotDatabaseSettings.Value.MonsterCollectionName
+        _monsterCollection = kotDatabaseSettings.Value.MonsterCollectionName;
+        _dbInstance = dbInstance;
+    }
+
+    public IEnumerable<Element> Create(MonsterPayload payload)
+    {
+        MonsterPayload args = (MonsterPayload)payload;
+        var newMonster = new Monster((string)args.Name!, (int)args.VictoryPoints!, (int)args.LifePoints!);
+        _dbInstance.InsertOne<Monster>(
+            collectionName: _monsterCollection,
+            document: newMonster
         );
+        return new List<Monster> { newMonster };
     }
 
-    IEnumerable<Element> IMonsterService.Create(MonsterPayload payload)
-    {
-        if (payload.GetType() == typeof(MonsterPayload))
-        {
-            MonsterPayload args = (MonsterPayload)payload;
-            var newMonster = new Monster((string)args.Name!, (int)args.VictoryPoints!, (int)args.LifePoints!);
-
-            MonsterCollection.InsertOne(newMonster);
-            return new Element[] { newMonster };
-        }
-
-        return new Element[0];
-    }
-
-    IEnumerable<Element> IMonsterService.Read(MonsterPayload payload)
+    public IEnumerable<Element> Read(MonsterPayload payload)
     {
         if (payload.Id != null)
         {
-            return MonsterCollection.Find(x => x.Id!.Equals(payload.Id)).ToList();
+            var filter = _dbInstance.GetFilterId<Monster>(payload.Id);
+            return _dbInstance.Find<Monster>(
+                collectionName: _monsterCollection,
+                filter: filter
+            );
         }
 
-        return MonsterCollection.Find(x => true).ToList();
+        return _dbInstance.Find<Monster>(collectionName: _monsterCollection, null);
     }
 
-    IEnumerable<Element> IMonsterService.Update(MonsterPayload payload)
-    {
-        if (payload.Id != null || payload.GetType() == typeof(MonsterPayload))
-        {
-            MonsterPayload args = (MonsterPayload)payload;
-            var newMonster = new Monster((string)args.Name!, (int)args.VictoryPoints!, (int)args.LifePoints!);
-
-            var filter = Builders<Monster>.Filter.Eq(monster => monster.Id, payload.Id);
-            var update = Builders<Monster>.Update
-                .Set(monster => monster.Name, newMonster.Name)
-                .Set(monster => monster.VictoryPoints, newMonster.VictoryPoints)
-                .Set(monster => monster.LifePoints, newMonster.LifePoints);
-
-            MonsterCollection.UpdateOne(filter, update);
-
-            return new Element[] { newMonster };
-        }
-
-        return new Element[0];
-    }
-
-    IEnumerable<Element> IMonsterService.Delete(MonsterPayload payload)
+    public IEnumerable<Element> Delete(MonsterPayload payload)
     {
         if (payload.Id != null)
         {
-            var deleted = MonsterCollection.FindOneAndDelete(x => x.Id!.Equals(payload.Id));
-            return new Element[] { deleted };
+            var filter = _dbInstance.GetFilterId<Monster>(payload.Id);
+            var deleted = _dbInstance.Delete<Monster>(
+                collectionName: _monsterCollection,
+                filter: filter
+            );
+            return new List<Monster> { deleted };
         }
 
+        return new List<Monster> { };
+    }
+
+    public IEnumerable<Element> Update(MonsterPayload payload)
+    {
         return new Element[0];
     }
 }
--- KOF/GameTest/ServicesTest/Elements/MonsterServiceTest.cs
+++ KOF/GameTest/ServicesTest/Elements/MonsterServiceTest.cs
@@ -1,58 +1,192 @@
-﻿// namespace KOT.Services.Test;
+﻿namespace KOT.Services.Test;
 
-// using KOT.Models;
-// using KOT.Services;
+using KOT.Models;
+using KOT.Models.Abstracts;
+using KOT.Services;
+using KOT.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Moq;
 
-// public class MonsterServiceTest
-// {
-//     [Fact]
-//     public void ShouldReturnAMonster()
-//     {
-//         MonsterService _service = new MonsterService();
+public class MonsterServiceMockData
+{
+    public static List<Monster>? GetListMock()
+    {
+        return new List<Monster>
+        {
+            new Monster("name1", 1, 0),
+            new Monster("name2", 1, 0),
+            new Monster("name3", 1, 0),
+        };
+    }
 
-//         Assert.Equal("CyberKitty", _service.Read(1)?.Name);
-//         Assert.Equal("Gigazaur", _service.Read(2)?.Name);
-//     }
+    public static List<Monster>? GetSingleMock()
+    {
+        return new List<Monster> { new Monster("name1", 1, 0) };
+    }
 
-//     [Fact]
-//     public void ShouldReturnAListWithAllMonsters()
-//     {
-//         MonsterService _service = new MonsterService();
+    public static void DoNothing()
+    {
+        //Does nothing for mocking purposes
+    }
+}
 
-//         Assert.IsType<List<Monster>>(_service.Read());
-//         Assert.True(_service.Read().Any());
-//     }
+public class MonsterServiceTests
+{
+    [Fact]
+    public void TestReadMethodWithId()
+    {
+        //Set expected result
+        var expected = MonsterServiceMockData.GetSingleMock();
 
-//     [Fact]
-//     public void ShouldSuccessfullyAddAMonsterToTheList()
-//     {
-//         MonsterService _service = new MonsterService();
-//         Assert.Equal(2, _service.Read().Count);
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.MonsterCollectionName = "Monsters";
+        var options = Options.Create(settings);
 
-//         service.Add(new Monster("New Monster", 10, 10));
-//         Assert.Equal(3, _service.Read().Count);
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+        var filterMock = new Mock<FilterDefinition<Monster>>();
 
-//         service.Add(new Monster("Another New Monster", 10, 10));
-//         Assert.Equal(4, _service.Read().Count);
-//     }
+        //Setup database service patches
+        dbMock.Setup(x => x.GetFilterId<Monster>(It.IsAny<string>())).Returns(filterMock.Object);
+        dbMock
+            .Setup(x => x.Find<Monster>(It.IsAny<string>(), filterMock.Object))
+            .Returns(expected!);
 
-//     [Fact]
-//     public void ShouldSuccessfullyUpdateAMonsterInTheList()
-//     {
-//         MonsterService _service = new MonsterService();
-//         Assert.Equal("CyberKitty", _service.Read(1)?.Name);
+        //Initialize service
+        var service = new MonsterService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new MonsterPayload(id: "Non null");
 
-//         service.Update(new Monster("New Monster", 10, 10));
-//         Assert.Equal("New Monster", _service.Read(1)?.Name);
-//     }
+        var result = service.Read(payload: payload);
 
-//     [Fact]
-//     public void ShouldSuccessfullyDeleteAMonsterInTheList()
-//     {
-//         MonsterService _service = new MonsterService();
-//         Assert.Equal(2, _service.Read()?.Count);
+        Assert.Equal(typeof(List<Monster>), expected!.GetType());
+        Assert.Equal(expected: expected, actual: result);
+    }
 
-//         _service.Delete(1);
-//         Assert.Equal(1, _service.Read()?.Count);
-//     }
-// }
+    [Fact]
+    public void TestReadMethodWithoutId()
+    {
+        //Set expected result
+        var expected = MonsterServiceMockData.GetListMock();
+
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.MonsterCollectionName = "Monsters";
+        var options = Options.Create(settings);
+
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+        var filterMock = new Mock<FilterDefinition<Monster>>();
+
+        //Setup database service patches
+        dbMock.Setup(x => x.GetFilterId<Monster>(It.IsAny<string>())).Returns(filterMock.Object);
+        dbMock.Setup(x => x.Find<Monster>(It.IsAny<string>(), null)).Returns(expected!);
+
+        //Initialize service
+        var service = new MonsterService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new MonsterPayload();
+
+        var result = service.Read(payload: payload);
+
+        Assert.Equal(typeof(List<Monster>), expected!.GetType());
+        Assert.Equal(expected: expected, actual: result);
+    }
+
+    [Fact]
+    public void TestCreateMethodExpectedElement()
+    {
+        //Set expected result
+        var expected = MonsterServiceMockData.GetSingleMock();
+
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.MonsterCollectionName = "Monsters";
+        var options = Options.Create(settings);
+
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+
+        //Setup database service patches
+        dbMock
+            .Setup(x => x.InsertOne<Monster>(It.IsAny<string>(), It.IsAny<Monster>()))
+            .Callback(MonsterServiceMockData.DoNothing);
+
+        //Initialize service
+        var service = new MonsterService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new MonsterPayload(name: "name1", victoryPoints: 1, lifePoints: 0);
+
+        var result = service.Create(payload: payload);
+
+        Assert.Equal(typeof(List<Monster>), expected!.GetType());
+        Assert.Single(collection: result);
+    }
+
+    [Fact]
+    public void TestDeleteMethodWithId()
+    {
+        //Set expected result
+        var expected = MonsterServiceMockData.GetSingleMock();
+
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.MonsterCollectionName = "Monsters";
+        var options = Options.Create(settings);
+
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+        var filterMock = new Mock<FilterDefinition<Monster>>();
+
+        //Setup database service patches
+        dbMock.Setup(x => x.GetFilterId<Monster>(It.IsAny<string>())).Returns(filterMock.Object);
+        dbMock
+            .Setup(x => x.Delete<Monster>(It.IsAny<string>(), filterMock.Object))
+            .Returns(expected!.First());
+
+        //Initialize service
+        var service = new MonsterService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new MonsterPayload(id: "Non null");
+
+        var result = service.Delete(payload: payload);
+
+        Assert.Equal(typeof(List<Monster>), expected!.GetType());
+        Assert.Equal(expected: expected, actual: result);
+    }
+
+    [Fact]
+    public void TestDeleteMethodWithoutId()
+    {
+        //Set expected result
+        var expected = new List<Monster> { };
+
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.MonsterCollectionName = "Monsters";
+        var options = Options.Create(settings);
+
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+
+        //Initialize service
+        var service = new MonsterService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new MonsterPayload();
+
+        var result = service.Delete(payload: payload);
+
+        Assert.Equal(typeof(List<Monster>), expected!.GetType());
+        Assert.Empty(collection: result);
+    }
+
+    [Fact]
+    public void TestUpdateMethod()
+    {
+        var settings = new KOTDatabaseSettings();
+        var options = Options.Create(settings);
+        var dbMock = new Mock<IDatabaseService>();
+        var service = new MonsterService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new MonsterPayload();
+
+        Assert.IsType<Element[]>(service.Update(payload));
+
+    }
+}
