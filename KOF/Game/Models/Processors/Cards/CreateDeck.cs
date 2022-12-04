4cf19dff703ce5cc5faa06d64e56ebff47522b2d
unknown <svaleria.gonzales@gmail.com>
2022-12-04 13:09:45 -0400

    Create deck and unit test created

--- KOF/Game/Models/Processors/Cards/CreateDeck.cs
+++ KOF/Game/Models/Processors/Cards/CreateDeck.cs
@@ -4,16 +4,21 @@ using KOT.Models.Abstracts;
 using KOT.Models.Processor;
 public class PowerCardDeck
 {
-    // public readonly List<PowerCard> Deck;
-    // public PowerCardDeck()
-    // {
-    //     Deck = new List<PowerCard>();
-    //     Deck.Add(new PowerCard("Alien Metabolism", 3, 0, new Effect(costReduction: 1))); //buying cards costs you 1 less
-    //     Deck.Add(new PowerCard("Jet Fighters", 5, 1, new Effect(star: 5, damage: 4))); //"+ 5[Star] and take 4 damage"
-    //     Deck.Add(new PowerCard("Apartament Building", 5, 1, new Effect(star: 3))); // + 3 stars
-    //     Deck.Add(new PowerCard("Energize", 8, 1, new Effect(energy: 9))); // + 9 energy
-    //     Deck.Add(new PowerCard("Heal", 3, 1,new Effect(heart: 2))); // heal 2 damage
-    //     Deck.Add(new PowerCard("Skycraper", 6, 1, new Effect(star: 4))); // +4 stars
-    //     Deck.Add(new PowerCard("Corner Store", 3, 1, new Effect(star: 1))); // +1 star
-    // }
+    public List<CardDetails> Deck = new List<CardDetails>();
+    public PowerCardDeck()
+    {
+        Deck.Add(new CardDetails(new PowerCard("Alien Metabolism", 3, 0), new Effect(costReduction: 1))); //buying cards costs you 1 less
+        Deck.Add(new CardDetails(new PowerCard("Jet Fighters", 5, 1), new Effect(starPoints: 5, damagePoints: 4))); //"+ 5[Star] and take 4 damage"
+        Deck.Add(new CardDetails(new PowerCard("Apartament Building", 5, 1), new Effect(starPoints: 3)));// + 3 stars
+        Deck.Add(new CardDetails(new PowerCard("Energize", 8, 1), new Effect(energyPoints: 9))); // + 9 energy
+        Deck.Add(new CardDetails(new PowerCard("Heal", 3, 1), new Effect(heartPoints: 2))); // heal 2 damage
+        Deck.Add(new CardDetails(new PowerCard("Skycraper", 6, 1), new Effect(starPoints: 4))); // +4 stars
+        Deck.Add(new CardDetails(new PowerCard("Corner Store", 3, 1), new Effect(starPoints: 1))); // +1 star
+    }
+
+    public Effect GetEffect(string PowerCardName)
+    {
+        var PowerCardEffect = from cardDetails in Deck where cardDetails.powerCard.Name == PowerCardName  select cardDetails.effect;
+        return (Effect)PowerCardEffect.ToList()[0];
+    }
 }
--- KOF/GameTest/ModelsTest/Processors/PowerCardDeckTest.cs
+++ KOF/GameTest/ModelsTest/Processors/PowerCardDeckTest.cs
@@ -1,13 +1,37 @@
-namespace KOT.Models.TestDeck;
+namespace KOT.Models.Test;
+
+using KOT.Models.Processor;
 using KOT.Services.Processors;
-using KOT.Models;
+
 public class PowerCardDeckTest
 {
     [Fact]
-    public void ShouldReturnAPowerCard()
+    public void ShouldReturnCardDetails()
     {
         PowerCardDeck powerCardDeck = new PowerCardDeck();
-        // Assert.IsType<PowerCard>(powerCardDeck.Deck.First());
+        Assert.IsType<CardDetails>(powerCardDeck.Deck.First());
     }
 
+    [Fact]
+    public void ShouldReturnEffectsForAPowerCard()
+    {
+        PowerCardDeck powerCardDeck = new PowerCardDeck();
+        var heartPoints =
+            from cardDetails in powerCardDeck.Deck
+            where cardDetails.powerCard.Name == "Heal"
+            select cardDetails.effect.HeartPoints;
+        Assert.Equal(2, heartPoints.ToList()[0]);
+    }
+
+    [Fact]
+    public void ShouldReturnEffectsForAGivenPowerCardName()
+    {
+        PowerCardDeck powerCardDeck = new PowerCardDeck();
+        Effect powerCardEffect = powerCardDeck.GetEffect("Heal");
+        Assert.Equal(2, powerCardEffect.HeartPoints);
+
+        Effect powerCardEffect1 = powerCardDeck.GetEffect("Jet Fighters");
+        Assert.Equal(5, powerCardEffect1.StarPoints);
+        Assert.Equal(4, powerCardEffect1.DamagePoints);
+    }
 }
