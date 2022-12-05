3e3e55b67dbb27d5a39aad1bd826ada65052d6ad
Beyta Gonzales <svaleria.gonzales@gmail.com>
2022-12-05 13:14:47 -0400

    test create method for player

--- KOF/GameTest/ServicesTest/Elements/PlayerServiceTest.cs
+++ KOF/GameTest/ServicesTest/Elements/PlayerServiceTest.cs
@@ -62,4 +62,61 @@ public class PlayerServiceTest
         Assert.Equal(typeof(List<Player>), expected!.GetType());
         Assert.Equal(expected: expected, actual: result);
     }
+    [Fact]
+    public void TestReadMethodWithoutId()
+    {
+        //Set expected result
+        var expected = PlayerServiceMockData.GetListMock();
+
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.PlayerCollectionName = "Players";
+        var options = Options.Create(settings);
+
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+        var filterMock = new Mock<FilterDefinition<Player>>();
+
+        //Setup database service patches
+        dbMock.Setup(x => x.GetFilterId<Player>(It.IsAny<string>())).Returns(filterMock.Object);
+        dbMock.Setup(x => x.Find<Player>(It.IsAny<string>(), null)).Returns(expected!);
+
+        //Initialize service
+        var service = new PlayerService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new PlayerPayload();
+
+        var result = service.Read(payload: payload);
+
+        Assert.Equal(typeof(List<Player>), expected!.GetType());
+        Assert.Equal(expected: expected, actual: result);
+    }
+    [Fact]
+    public void TestCreateMethodExpectedElement()
+    {
+        //Set expected result
+        var expected = PlayerServiceMockData.GetSingleMock();
+
+        // Service settings
+        var settings = new KOTDatabaseSettings();
+        settings.PlayerCollectionName = "Players";
+        var options = Options.Create(settings);
+
+        // Mock database service instance
+        var dbMock = new Mock<IDatabaseService>();
+
+        //Setup database service patches
+        dbMock
+            .Setup(x => x.InsertOne<Player>(It.IsAny<string>(), It.IsAny<Player>()))
+            .Callback(PlayerServiceMockData.DoNothing);
+
+        //Initialize service
+        var service = new PlayerService(kotDatabaseSettings: options, dbInstance: dbMock.Object);
+        var payload = new PlayerPayload(name: "player1", myMonster: new Monster("monster1", 10, 10), energyCubes: 14);
+
+        var result = service.Create(payload: payload);
+
+        Assert.Equal(typeof(List<Player>), expected!.GetType());
+        Assert.Single(collection: result);
+    }
+
 }
