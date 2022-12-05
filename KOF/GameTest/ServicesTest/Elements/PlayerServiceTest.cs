d4bbb18580f463f72f1dc30ce3af840b6e0b8002
Beyta Gonzales <svaleria.gonzales@gmail.com>
2022-12-05 11:35:18 -0400

    player service mock data created at player service test

--- KOF/GameTest/ServicesTest/Elements/PlayerServiceTest.cs
+++ KOF/GameTest/ServicesTest/Elements/PlayerServiceTest.cs
@@ -4,60 +4,34 @@ using KOT.Models;
 using KOT.Models.Abstracts;
 using KOT.Services;
 using KOT.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Moq;
+
+public class PlayerServiceMockData
+{
+    public static List<Player>? GetListMock()
+    {
+        return new List<Player>
+        {
+            new Player("player1", new Monster("monster1", 10, 10), 14),
+            new Player("player2", new Monster("monster2", 9, 9), 20),
+            new Player("player3", new Monster("monster3", 8, 8), 30),
+        };
+    }
+
+    public static List<Player>? GetSingleMock()
+    {
+        return new List<Player> { new Player("player1", new Monster("monster1", 10, 10), 14) };
+    }
+
+    public static void DoNothing()
+    {
+        //Does nothing for mocking purposes
+    }
+}
 
 public class PlayerServiceTest
 {
-    // private readonly IDataService? playerService;
-    // [Fact]
-    // public void ShouldReturnAListWithAllPlayers()
-    // {
-    //     // var x = playerService.Read(new PlayerPayload {});
-    //     // Assert.IsType<List<Player>>(x);
-    // }
-
-    // [Fact]
-    // public void ShouldReturnAPlayerByPID()
-    // {
-    //     PlayerService playerService = new PlayerService();
-    //     Assert.False(object.ReferenceEquals(null, playerService.Get(1)));
-    //     Assert.False(object.ReferenceEquals(null, playerService.Get(2)));
-    // }
-
-    // [Fact]
-    // public void ShouldReturnNonNullObjectWhenANewPlayerIsCreated()
-    // {
-    //     PlayerService playerService = new PlayerService();
-    //     playerService.Add(
-    //         new Player()
-    //         {
-    //             PID = 3,
-    //             Name = "Kive",
-    //             MyMonster = new Monster()
-    //         }
-    //     );
-    //     Assert.False(object.ReferenceEquals(null, playerService.Get(3)));
-    // }
-
-    // [Fact]
-    // public void ShouldReturnNullWhenAPlayerIsDeleted()
-    // {
-    //     PlayerService playerService = new PlayerService();
-    //     playerService.Delete(2);
-    //     Assert.True(object.ReferenceEquals(null, playerService.Get(2)));
-    // }
-
-    // [Fact]
-    // public void ShouldReturnNewNameWhenAPlayerIsUpdated()
-    // {
-    //     PlayerService playerService = new PlayerService();
-    //     Assert.Equal("Pablo", playerService.Get(2)?.Name);
-    //     Player player = new Player()
-    //     {
-    //         PID = 2,
-    //         Name = "Jose",
-    //         MyMonster = new Monster()
-    //     };
-    //     playerService.Update(player);
-    //     Assert.Equal("Jose", playerService.Get(2)?.Name);
-    // }
+    
 }
