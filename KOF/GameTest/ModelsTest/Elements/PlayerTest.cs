483daa42d805d170a8f555e964ab8d6e953449bb
Beyta Gonzales <svaleria.gonzales@gmail.com>
2022-12-05 20:50:16 -0400

    Player test, energycubes and powercards tests added

--- KOF/GameTest/ModelsTest/Elements/PlayerTest.cs
+++ KOF/GameTest/ModelsTest/Elements/PlayerTest.cs
@@ -5,7 +5,7 @@ namespace KOT.Models.Test;
 
 public class PlayerTest
 {
-    private readonly Player player1 = new Player("Pablo", new Monster("monster1", 10, 10));
+    private readonly Player player1 = new Player("Pablo", new Monster("monster1", 10, 10), 14, new List<PowerCard>(){ new PowerCard("powerCard1", 10, 1)});
 
     [Fact]
     public void ShouldReturnPropertyNameAsString()
@@ -24,5 +24,30 @@ public class PlayerTest
     {
         Player player2 = new Player("Player2", null!);
         Assert.True(object.ReferenceEquals(null, player2.MyMonster));
+    }
+    [Fact]
+    public void ShouldReturnEnergyCubesAsIntIsDeclared()
+    {
+        Assert.Equal(14, player1.EnergyCubes);
+    }
+
+    [Fact]
+    public void ShouldReturnEnergyCubesAsZeroWhenEnergyCubesIsNotDeclared()
+    {
+        Player player2 = new Player("Player2", null!);
+        Assert.Equal(0, player2.EnergyCubes);
+    }
+
+    [Fact]
+    public void ShouldReturnPowerCardsAsListOfPowercards()
+    {
+        Assert.IsType<List<PowerCard>>(player1.PowerCards);
+        Assert.False(object.ReferenceEquals(null, player1.PowerCards));
+    }
+     [Fact]
+    public void ShouldReturnPowerCardsAsNull()
+    {
+        Player player2 = new Player("Player2", null!);
+        Assert.True(object.ReferenceEquals(null, player2.PowerCards));
     }
 }
